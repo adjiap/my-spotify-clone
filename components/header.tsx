b904b36176e06c4471adc6b1cc632f4b88f2a1c8
adjiap <20769489+adjiap@users.noreply.github.com>
2024-07-01 13:32:10 +0200

    API: Create Header component

--- components/header.tsx
+++ components/header.tsx
@@ -2,7 +2,6 @@
 
 import { useRouter } from "next/navigation";
 import React from "react";
-import { RxCaretLeft } from "react-icons/rx";
 import { twMerge } from "tailwind-merge";
 
 interface HeaderProps{
@@ -29,35 +28,6 @@ const Header: React.FC<HeaderProps> = (
             className
         )}
         >
-            <div className="
-                w-full
-                mb-4
-                flex
-                items-center
-                justify-between
-            ">
-                <div className="
-                    hidden
-                    md:flex
-                    gap-x-2
-                    items-center
-                ">
-                    <button className="
-                        rounded-full
-                        bg-black
-                        flex
-                        items-center
-                        justify-center
-                        hover:opacity-75
-                        transition
-                    ">
-                        <RxCaretLeft
-                            className="text-white"
-                            size={35}
-                        />
-                    </button>
-                </div>
-            </div>
         </div>
     )
 }
