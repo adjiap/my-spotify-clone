a645eff4a0b577ba50311fa0dc3455052ac85094
adjiap <20769489+adjiap@users.noreply.github.com>
2024-07-03 15:36:40 +0200

    API: Add SupabaseProvider and wrap application with it

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import { Figtree } from "next/font/google";
 import "./globals.css";
 import Sidebar from "@/components/sidebar"
-import SupabaseProvider from "@/providers/SupabaseProviders";
+import SupabaseProvider from "@/providers/SupabaseProvider";
 
 const font = Figtree({ subsets: ["latin"] });
 
