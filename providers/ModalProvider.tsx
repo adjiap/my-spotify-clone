276b08b8213d8fd9f4bd302b613824560e017cbc
adjiap <20769489+adjiap@users.noreply.github.com>
2024-07-03 15:44:13 +0200

    API: Create ModalProvider

---
+++ providers/ModalProvider.tsx
@@ -0,0 +1,24 @@
+"use client";
+
+import React, { useEffect, useState } from "react";
+
+const ModalProvider = () => {
+    const [isMounted, setIsMounted] = useState(false);
+    
+    // Prevents errors by modals due to server-side rendering, as modals can cause errors
+    useEffect(() => {
+        setIsMounted(true);
+    }, []);
+
+    if (!isMounted){
+        return null;
+    }
+
+    return (
+        <>
+            Modals!
+        </>
+    )
+}
+
+export default ModalProvider;
