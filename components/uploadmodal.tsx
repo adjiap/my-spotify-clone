e1a9fd496e931d75ea4eae3f0c7f23d4958ae79e
adjiap <20769489+adjiap@users.noreply.github.com>
2024-07-05 16:31:24 +0200

    ENH: Add input boxes and submit button for songs to uploadModal

--- components/uploadmodal.tsx
+++ components/uploadmodal.tsx
@@ -5,6 +5,7 @@ import Modal from "./modal";
 import useUploadModal from "@/hooks/useUploadModal";
 import { FieldValues, SubmitHandler, useForm } from "react-hook-form";
 import Input from "./input";
+import Button from "./button";
 
 const uploadModal = () => {
     const uploadModal = useUploadModal();
@@ -42,14 +43,43 @@ const uploadModal = () => {
         >
             <form
                 onSubmit={handleSubmit(onSubmit)}
+                className="flex flex-col gap-y-4" // Add separation between input boxes
             >
                 <Input
                     id="title"
                     disabled={isLoading}
                     {...register("title", { required:true })}  // must be the same as id
                     placeholder="Song Title"
-                >
-                </Input>
+                />
+                <Input
+                    id="author"
+                    disabled={isLoading}
+                    {...register("author", { required:true })}
+                    placeholder="Song author"
+                />
+                <div className="pb-1">
+                    Select a song from file
+                    <Input
+                        id="song"
+                        type="file"
+                        disabled={isLoading}
+                        accept=".mp3"       // would only accept .mp3 files
+                        {...register("song", { required:true })}
+                    />
+                </div>
+                <div className="pb-1">
+                    Select an image
+                    <Input
+                        id="image"
+                        type="file"
+                        disabled={isLoading}
+                        accept="image/*"   // would only accept files that are images in nature, like .png or .jpeg
+                        {...register("image", { required:true })} 
+                    />
+                </div>
+                <Button disabled={isLoading} type="submit">
+                    Add Song
+                </Button>
             </form>
         </Modal>
     )
