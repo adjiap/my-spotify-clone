b9c9822482c38e997d83ea5954ffb9dd63a091a9
adjiap <20769489+adjiap@users.noreply.github.com>
2024-07-05 10:39:36 +0200

    API: Update login modal with authentification steps

--- components/authmodal.tsx
+++ components/authmodal.tsx
@@ -2,18 +2,53 @@
 
 import React from "react";
 import Modal from "./modal";
+import {
+    useSessionContext,
+    useSupabaseClient
+} from "@supabase/auth-helpers-react";
+import { useRouter } from "next/router";
+import { Auth } from "@supabase/auth-ui-react";
+import { ThemeSupa } from "@supabase/auth-ui-shared";
+import useAuthModal from "@/hooks/useAuthModal";
 
 const AuthModal = () => {
+    const supabaseClient = useSupabaseClient();
+    const router = useRouter;
+    const { session } = useSessionContext();
+    const { onClose, isOpen } = useAuthModal(); 
+
+    const onChange = (open: boolean) => {
+        if (!open){
+            onClose();
+        }
+    }
+
     return(
         <Modal
             title="Welcome back"
             description="Login to your account"
-            isOpen
-            onChange={() => {}}
+            isOpen={isOpen}
+            onChange={onChange}
         >
-           Auth modal 
+           <Auth
+            theme="dark" // theme of the pop up for login
+            magicLink      // login using e-mails
+            providers={["github"]}  // which providers should be shown during login, that's allowed for logging in. Default would've been Github, Google and Microsoft Azure for Supabase
+            supabaseClient={supabaseClient}  // client object to be used
+            appearance={{
+                theme: ThemeSupa,   // overall appearance of theme
+                variables: {
+                    default: {
+                        colors: {
+                            brand: "#404040",
+                            brandAccent: "#22c55e"
+                        }
+                    }
+                }
+            }}
+           /> 
         </Modal>
-    )
-};
+    );
+}
 
 export default AuthModal;
