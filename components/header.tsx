61adf46b0bdccfa39625249a37a8731831f0bfea
adjiap <20769489+adjiap@users.noreply.github.com>
2024-07-05 14:47:33 +0200

    ENH: Implement ToastProvider for logging out process

--- components/header.tsx
+++ components/header.tsx
@@ -11,6 +11,7 @@ import useAuthModal from "@/hooks/useAuthModal";
 import { useSupabaseClient } from "@supabase/auth-helpers-react";
 import { useUser } from "@/hooks/useUsers";
 import { FaUserAlt } from "react-icons/fa";
+import toast from "react-hot-toast";
 
 interface HeaderProps{
     children: React.ReactNode;
@@ -32,7 +33,9 @@ const Header: React.FC<HeaderProps> = ({
         // TODO: reset any playing songs
         router.refresh();
         if (error){
-            console.log(error);
+            toast.error(error.message);
+        } else {
+            toast.success("Logged out!");
         }
     }
 
