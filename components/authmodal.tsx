a721edb171cde3c1e34dd6b44572e7344db8bb5b
adjiap <20769489+adjiap@users.noreply.github.com>
2024-07-05 12:56:57 +0200

    ENH: Add authentication step that will close Auth modal popup

--- components/authmodal.tsx
+++ components/authmodal.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React from "react";
+import React, { useEffect } from "react";
 import Modal from "./modal";
 import {
     useSessionContext,
@@ -16,6 +16,14 @@ const AuthModal = () => {
     const router = useRouter();
     const { session } = useSessionContext();
     const { onClose, isOpen } = useAuthModal(); 
+
+    // When user signs in successfully, the modal will close
+    useEffect(() => {
+        if (session) {
+            router.refresh();
+            onClose();
+        }
+    }, [session, router, onClose]);  // The dependency array
 
     const onChange = (open: boolean) => {
         if (!open){
