4e200885b34c12a3100aa8f000bf327107a0fa5c
adjiap <20769489+adjiap@users.noreply.github.com>
2024-07-18 11:35:32 +0200

    ENH: Implement SearchInput to create input box on Search page

--- components/searchinput.tsx
+++ components/searchinput.tsx
@@ -1,12 +1,34 @@
 "use client";
 
-import React from "react";
+import qs from "query-string"
+import useDebounce from "@/hooks/useDebounce";
+import { useRouter } from "next/navigation";
+import React, { useEffect, useState } from "react";
+import Input from "./input";
 
 const SearchInput = () => {
+    const router= useRouter();
+    const [value, setValue] = useState<string>("");
+    const debouncedValue = useDebounce<string>(value, 500);
+
+    useEffect(() => {
+        const query = {
+            title: debouncedValue,
+        };
+
+        const url = qs.stringifyUrl({
+            url: '/search',
+            query: query
+        })
+
+        router.push(url);
+    }, [debouncedValue, router]);
     return (
-        <div>
-            Input Here
-        </div>
+        <Input
+            placeholder="What do you want to listen to?"
+            value={value}
+            onChange={(e) => setValue(e.target.value)}
+        />
     );
 };
 
