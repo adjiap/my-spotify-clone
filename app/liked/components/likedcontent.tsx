8b0d699ba911140d929dc56bf96c79dece559998
adjiap <20769489+adjiap@users.noreply.github.com>
2024-07-18 15:06:20 +0200

    API: Add placeholder for LikedContent component

---
+++ app/liked/components/likedcontent.tsx
@@ -0,0 +1,13 @@
+"use client";
+
+import React from "react";
+
+const LikedContent = () => {
+    return (
+        <div>
+            Liked Content
+        </div>
+    )
+}
+
+export default LikedContent;
