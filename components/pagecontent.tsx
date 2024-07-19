1528412c24804300bd4e4092203d519abe4eab1a
adjiap <20769489+adjiap@users.noreply.github.com>
2024-07-19 11:08:55 +0200

    ENH: Apply the display of running song to main page

--- components/pagecontent.tsx
+++ components/pagecontent.tsx
@@ -3,6 +3,7 @@
 import { Song } from "@/types";
 import React from "react";
 import SongItem from "./songitem";
+import useOnPlay from "@/hooks/useOnPlay";
 
 interface PageContentProps {
     songs: Song[];
@@ -11,6 +12,8 @@ interface PageContentProps {
 const PageContent: React.FC<PageContentProps> = ({
     songs
 }) => {
+    const onPlay = useOnPlay(songs)
+
     if (songs.length === 0) {
         return (
             <div className="mt-4 text-neutral-400">
@@ -35,7 +38,7 @@ const PageContent: React.FC<PageContentProps> = ({
             {songs.map((item) => (
                 <SongItem
                     key={item.id}
-                    onClick={() => {}}
+                    onClick={(id:string) => onPlay(id)}
                     data={item}
                 />
             ))}
