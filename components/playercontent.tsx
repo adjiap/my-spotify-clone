ff908948845a2724f21405642c1e8e67dae35091
adjiap <20769489+adjiap@users.noreply.github.com>
2024-07-19 12:06:56 +0200

    ENH: Add the play and skip buttons on PlayerContent

--- components/playercontent.tsx
+++ components/playercontent.tsx
@@ -20,6 +20,7 @@ const PlayerContent: React.FC<PlayerContentProps> = ({
 }) => {
     // TODO: remove hardcoded `false` that will permanently show a Play button in mobile view
     const Icon = false ? BsPauseFill : BsPlayFill;
+    const VolumeIcon = true ? HiSpeakerXMark : HiSpeakerWave
 
     return (
         <div className="grid grid-cols-2 md:grid-cols-3 h-full">
@@ -58,6 +59,65 @@ const PlayerContent: React.FC<PlayerContentProps> = ({
                     "
                 >
                     <Icon size={30} className="text-black"/>
+                </div>
+            </div>
+            {/* This will be the backward, forward, and play button */}
+            <div
+                className="
+                    hidden
+                    h-full
+                    md:flex
+                    justify-center
+                    items-center
+                    w-full
+                    max-w-[722px]
+                    gap-x-6
+                "
+            >
+                <AiFillStepBackward
+                    size={30}
+                    onClick = {() => {}}
+                    className="
+                        text-neutral-400
+                        cursor-pointer
+                        hover:text-white
+                        transition
+                    "
+                    />
+                <div
+                    onClick={() => {}}
+                    className="
+                        flex
+                        items-center
+                        justify-center
+                        h-10
+                        w-10
+                        rounded-full
+                        bg-white
+                        p-1
+                        cursor-pointer
+                    "
+                >
+                    <Icon size={30} className="text-black" />
+                </div>
+                <AiFillStepForward
+                    size={30}
+                    onClick = {() => {}}
+                    className="
+                        text-neutral-400
+                        cursor-pointer
+                        hover:text-white
+                        transition
+                    "
+                />
+            </div>
+            <div className="hidden md:flex w-full justify-end pr-2">
+                <div className="flex items-center gap-x-2 w-[120px]">
+                    <VolumeIcon 
+                        onClick={() => {}}
+                        className="cursor-pointer"
+                        size={34}
+                    />
                 </div>
             </div>
         </div>
