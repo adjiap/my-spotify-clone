d09442ff60bab3673202608c0627fe3fd1ed7f68
adjiap <20769489+adjiap@users.noreply.github.com>
2024-07-22 11:43:45 +0200

    STY: Fix missing semi-colons, remove whitespace

--- components/playercontent.tsx
+++ components/playercontent.tsx
@@ -32,12 +32,12 @@ const PlayerContent: React.FC<PlayerContentProps> = ({
         if (player.ids.length === 0) {
             return;
         }
-    
+
         const currentIndex = player.ids.findIndex((id) => id === player.activeId);
         const previousSong = player.ids[currentIndex - 1]
 
         if (!previousSong){
-            return player.setId(player.ids[player.ids.length - 1])
+            return player.setId(player.ids[player.ids.length - 1]);
         }
 
         player.setId(previousSong);
@@ -52,7 +52,7 @@ const PlayerContent: React.FC<PlayerContentProps> = ({
         const nextSong = player.ids[currentIndex + 1]
 
         if (!nextSong){
-            return player.setId(player.ids[0])
+            return player.setId(player.ids[0]);
         }
 
         player.setId(nextSong);
@@ -116,7 +116,8 @@ const PlayerContent: React.FC<PlayerContentProps> = ({
                 className="
                     flex
                     md:hidden
-                    col-autow-full
+                    col-auto
+                    w-full
                     justify-end
                     items-center
                 "
@@ -203,6 +204,6 @@ const PlayerContent: React.FC<PlayerContentProps> = ({
             </div>
         </div>
     )
-}
+};
 
 export default PlayerContent;
