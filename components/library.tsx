5eb3aea4fc69b9fbbc13dddeda60f9d9d517ad9e
adjiap <20769489+adjiap@users.noreply.github.com>
2024-07-23 15:17:47 +0200

    ENH: Include new subscription modal in library and play hook

--- components/library.tsx
+++ components/library.tsx
@@ -9,6 +9,7 @@ import { AiOutlinePlus } from "react-icons/ai";
 import { TbPlaylist } from "react-icons/tb";
 import MediaItem from "./mediaitem";
 import useOnPlay from "@/hooks/useOnPlay";
+import useSubscribeModal from "@/hooks/useSubscribeModal";
 
 interface LibraryProps {
     songs: Song[];
@@ -17,9 +18,10 @@ interface LibraryProps {
 const Library: React.FC<LibraryProps> = ({
     songs
 }) => {
+    const subscribeModal = useSubscribeModal();
     const authModal = useAuthModal();
     const uploadModal = useUploadModal();
-    const { user } = useUser();
+    const { user, subscription } = useUser();
 
     const onPlay = useOnPlay(songs);
 
@@ -27,8 +29,14 @@ const Library: React.FC<LibraryProps> = ({
         if (!user){
             return authModal.onOpen();
         }
+        
+        if (!subscription) {
+            return subscribeModal.onOpen()
+        }
+
         return uploadModal.onOpen();
     };
+    
     return (
         <div className="flex flex-col">
             <div className="
