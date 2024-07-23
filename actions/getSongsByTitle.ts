a8d97526d459a895237b5923c830083a4673124d
adjiap <20769489+adjiap@users.noreply.github.com>
2024-07-23 16:44:00 +0200

    FIX: Correct bug in search function

--- actions/getSongsByTitle.ts
+++ actions/getSongsByTitle.ts
@@ -16,7 +16,7 @@ const getSongsByTitle = async(title: string):  Promise<Song[]> => {
     const { data, error } = await supabase
         .from("songs")
         .select("*")
-        .ilike("title", `%$(title)%`)       // Gives a more precise search algorithm
+        .ilike("title", `%${title}%`)       // Gives a more precise search algorithm
         .order("created_at", {ascending: false});
 
         if (error){
